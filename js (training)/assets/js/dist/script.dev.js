a3b34e350657478d510c8992fa212cf568e82a35
Zoriana1306 <zoristar13@ukr.net>
2023-12-30 18:06:43 +0200

    lesson 20 - intro to JS

---
+++ js (training)/assets/js/dist/script.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// === ПРАВИЛА ===
+// Можливі символи: Aa - Zz 0-9 _ $
+// Не може починатися з цифри!
+var name = prompt("What is your name?");
+var isDev = confirm("Are you a dev?");
---
+++ js (training)/assets/js/script.js
@@ -0,0 +1,6 @@
+// === ПРАВИЛА ===
+// Можливі символи: Aa - Zz 0-9 _ $
+// Не може починатися з цифри!
+
+let name = prompt("What is your name?");
+let isDev = confirm("Are you a dev?");
